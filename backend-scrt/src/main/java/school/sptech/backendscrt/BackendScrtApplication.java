91b92d8b24305cd0aeec1737bd411a3fb87a7132
edu-correa <eduardo.correa@sptech.school>
2024-03-06 20:41:09 -0300

    feature: estrutura do projeto em springboot web adicionada

---
+++ backend-scrt/src/main/java/school/sptech/backendscrt/BackendScrtApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.backendscrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendScrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendScrtApplication.class, args);
+	}
+
+}
