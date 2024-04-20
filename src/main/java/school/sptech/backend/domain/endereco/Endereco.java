e3f69cb0c4300b74e4e969ddf248d9260984d16b
arthurRFmoraes <arthur.moraes@sptech.school>
2024-04-20 16:00:13 -0300

    feat: ListaObj, quickSort

--- src/main/java/school/sptech/backend/domain/endereco/Endereco.java
+++ src/main/java/school/sptech/backend/domain/endereco/Endereco.java
@@ -1,4 +1,22 @@
 package school.sptech.backend.domain.endereco;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String logradouro;
+    private String bairro;
+    private String numero;
+    private String cep;
 }
