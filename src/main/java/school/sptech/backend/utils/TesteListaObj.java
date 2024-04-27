95c0a1c4e4929a9861dc7fc988b4be7852b205b9
gabrielBmoraes <gabriel.moraes@sptech.school>
2024-04-27 13:04:57 -0300

    gb/fix/CadastroRotaRecursivo

--- src/main/java/school/sptech/backend/utils/TesteListaObj.java
+++ src/main/java/school/sptech/backend/utils/TesteListaObj.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import school.sptech.backend.api.produto.ProdutoController;
 import school.sptech.backend.domain.endereco.Endereco;
+import school.sptech.backend.domain.produto.Produto;
 import school.sptech.backend.service.endereco.EnderecoViaCep;
 
 public class TesteListaObj {
+
+
     
     public static void main(String[] args) {
 
+        menuInicial();
 
-        Scanner leitorInt = new Scanner(System.in);
+
+        Scanner leitor = new Scanner(System.in);
         
         System.out.println(
             """
@@ -20,7 +26,7 @@ public class TesteListaObj {
             """
         );
 
-        int numeroRuas = leitorInt.nextInt();
+        int numeroRuas = leitor.nextInt();
 
         if (numeroRuas <= 0) {
             System.out.println("Opção inválida");
@@ -63,6 +69,164 @@ public class TesteListaObj {
     }
 
 
+    public static void menuInicial(){
+
+        Scanner leitorInt = new Scanner(System.in);
+
+        System.out.println(
+                """
+                1- Cadastrar Nova Rota
+                2- Sair
+                """
+        );
+
+        int respostaUsuario = leitorInt.nextInt();
+
+
+        switch (respostaUsuario){
+
+            case 1:
+                cadastrarRota();
+                menuInicial();
+                break;
+
+            case 2:
+                System.out.println(
+                        "\nSaindo...."
+                );
+                break;
+
+            default:
+                System.out.println("\n Valor incorreto, por favor, tente novamente!\n");
+                menuInicial();
+        }
+    }
+
+    public static void cadastrarRota(){
+
+        Scanner leitor = new Scanner(System.in);
+        ArrayList<String> ruasDaRota = new ArrayList<>();
+        System.out.println(
+                """
+                Cadastro
+                
+                Nome da rota
+                """
+        );
+
+        String nomeRota = leitor.nextLine();
+
+        if (alterarNome(nomeRota, leitor)){
+            cadastrarRota();
+        }
+
+        int ordemRua = 0;
+        adicionarRua(++ordemRua, nomeRota, leitor, ruasDaRota);
+
+    }
+
+    public static boolean alterarNome(String nomeObjeto, Scanner leitor){
+        System.out.println("Nome da rota: " + nomeObjeto + "\nDeseja alterar o nome? S/N");
+
+        return leitor.nextLine().equals("S");
+
+    }
+
+
+    public static void adicionarRua(int quantidadeRua, String nomeRota, Scanner leitor, ArrayList ruasDaRota) {
+        System.out.println(nomeRota);
+        System.out.println("Rua " + quantidadeRua + "\n");
+        String rua = leitor.nextLine();
+
+        if (alterarNome(rua, leitor)) {
+            adicionarRua(quantidadeRua, nomeRota, leitor, ruasDaRota);
+        }
+
+        System.out.println(
+                """
+                        1- Adicionar outra rua
+                        2- Cadastrar Rota
+                        """
+        );
+
+        int escolhaUsuario = leitor.nextInt();
+
+        switch (escolhaUsuario) {
+            case 1:
+                ruasDaRota.add(rua);
+                adicionarRua(++quantidadeRua, nomeRota, leitor, ruasDaRota);
+                break;
+
+            case 2:
+                exibirCadastroRota(nomeRota, ruasDaRota);
+                break;
+        }
+    }
+
+
+    public static void exibirCadastroRota(String nomedaRota, ArrayList ruasDaRota){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Rota cadastrada: " + nomedaRota + "\nRuas cadastradas: ");
+
+        for (int i = 0; i < ruasDaRota.size(); i++) {
+            System.out.printf("%d - %s", i+1, ruasDaRota.get(i));
+        }
+
+        System.out.println(
+                """
+                       
+                """
+        );
+
+        System.out.println("Deseja alterar alguma rua? S/N");
+
+        if (leitor.nextLine().equals("S")){
+            alterarRota();
+        }
+
+        //Aqui salva no banco
+
+
+    }
+
+
+    public static  void alterarRota(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void menu(int numeroRuas){
 
         Scanner leitorInt = new Scanner(System.in);
@@ -88,6 +252,8 @@ public class TesteListaObj {
                 case 1:
                     
                     break;
+
+
             
                 default:
                     break;
