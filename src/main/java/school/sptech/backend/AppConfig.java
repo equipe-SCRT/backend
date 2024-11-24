2637393602245aab35d328130eb0f9df010f220b
arthurRFmoraes <arthur.moraes@sptech.school>
2024-11-24 15:21:49 -0300

    db user e password config properties

--- src/main/java/school/sptech/backend/AppConfig.java
+++ src/main/java/school/sptech/backend/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config.properties")
 public class AppConfig {
     // Configuration class
     
