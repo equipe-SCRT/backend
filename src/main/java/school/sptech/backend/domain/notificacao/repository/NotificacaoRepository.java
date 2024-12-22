68397d755cafeca2827e46985f0fff2e79dfffe4
KauanCursino <kauan.souza@sptech.school>
2024-12-22 18:30:59 -0300

    refactor: remoção da entidade notificação do projeto

--- src/main/java/school/sptech/backend/domain/notificacao/repository/NotificacaoRepository.java
+++
@@ -1,8 +0,0 @@
-package school.sptech.backend.domain.notificacao.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import school.sptech.backend.domain.notificacao.Notificacao;
-
-public interface NotificacaoRepository extends JpaRepository<Notificacao, Integer> {
-}
